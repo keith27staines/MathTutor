0a3ee43856b8e876dbe6532c3a4d06fb8466a7ea
Keith Staines <keith27staines@icloud.com>
2017-08-12 07:22:39 +0100

    Initial Commit

---
+++ MathTutor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MathTutor
+//
+//  Created by Keith on 12/08/2017.
+//  Copyright © 2017 Keith. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
